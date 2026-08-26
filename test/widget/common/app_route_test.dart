4add12ae75d8aab7adcd4505d79c1c7ad8cd4577
Jaehoon Yang <lljhms@naver.com>
2026-08-26 23:46:07 +0900

    test: SlideRoute 타입 검증 테스트를 CupertinoRouteTransitionMixin 기준으로 수정
    PageRouteBuilder에서 PageRoute+CupertinoRouteTransitionMixin으로
    전환하면서 기존 타입 단언 테스트가 깨짐

--- test/widget/common/app_route_test.dart
+++ test/widget/common/app_route_test.dart
@@ -1,4 +1,5 @@
 import 'package:feple/common/util/app_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -25,10 +26,10 @@ void main() {
       expect(find.text('다음 화면'), findsOneWidget);
     });
 
-    testWidgets('PageRouteBuilder 타입이다', (tester) async {
+    testWidgets('iOS 엣지 스와이프 뒤로가기를 지원하는 CupertinoRouteTransitionMixin 타입이다', (tester) async {
       final route = SlideRoute(builder: (_) => const Text('화면'));
 
-      expect(route, isA<PageRouteBuilder<dynamic>>());
+      expect(route, isA<CupertinoRouteTransitionMixin<dynamic>>());
     });
   });
 }
